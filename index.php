1d77ab6552fcba2cc0c0297cda6e4053bdfaaa09
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-01-29 14:16:18 +0100

    iujytrr

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 
+
 /**
  * 1. Créez une variable contenant une chaîne de caractères contenant du texte lorem ( 1 seule ligne suffit )
  * 2. Ajouter le contenu de cette variable dans un fichier avec la méthode de votre choix.
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
+
+$lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Vestibulum eleifend libero libero.";
+$file = fopen('monFichier.txt','c+b');
+fwrite($file,"contenue1\n");
+fclose($file);
+
 
 
 /**
@@ -15,20 +23,33 @@
  * --> Attention: les chaînes de caractères doivent s'ajouter à la suite du contenu déjà existant, pas d'écrasement.
  */
 // TODO Votre code ici
-
+$tableau1 = ["chaine 1", "chaine 2", "chaine 3","chaine 4","chaine 5",
+    "chaine 6","chaine 7","chaine 8","chaine 9","chaine 10"];
+fopen($file,'c+b');
+file_put_contents($file,$tableau1[2]);
+fclose($file);
 
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
-
+fopen($file,'c+b');
+function get_file_ext($file)
+{
+    $newFile = pathinfo($file, PATHINFO_EXTENSION);
+    echo $newFile;
+}
+fclose($file);
 
 
 /**
  * 6. Testez si le fichier 'toto' existe, sil n'existe pas, afficher un texte distant que ce fichier n'existe pas !
  */
 // TODO Votre code ici.
-
+file_exists("toto");
+if("toto" === false){
+    echo "ce fichier existe pas !";
+}
 
 /**
  * Super bonus.
@@ -36,3 +57,6 @@
  * Attention, il y a un piège avec les pointeurs, et une manière très simple de procéder... réfléchissez...
  */
 // TODO Votre code ici si vous faites le bonus.
+
+
+
